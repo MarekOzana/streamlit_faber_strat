5cb02ba5032fe0a93cc3315815ba0991856771c7
Marek Ozana <marek.ozana@gmail.com>
2023-12-25 11:57:45 +0100

    refactoring. ix name instead fof ticker

--- app.py
+++ app.py
@@ -15,26 +15,27 @@ import yfinance as yf
 import pandas as pd
 from typing import Literal
 import altair as alt
-import calendar
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide", page_title="Faber's Trend-Following Strategy")
+
+TICKER2NAME: dict[str, str] = {
+    "S&P 500": "^GSPC",
+    "OMXS Stockholm 30": "^OMX",
+    "Russell 2000": "^RUT",
+}
 
 
 @st.cache_data
-def download_data(ticker: Literal["^GSPC", "^OMX"]) -> pd.DataFrame:
+def download_data(ticker: Literal["^GSPC", "^OMX", "^RUT"]) -> pd.DataFrame:
     # Fetch monthly data
     data = yf.Ticker(ticker).history(period="max", interval="1mo")[["Close"]]
-    data = data.assign(
-        sma=lambda x: x["Close"].rolling(window=10).mean(),
-        pos=lambda x: (x["Close"].gt(x["sma"])).astype(int).shift(1),
-        ret=lambda x: x["Close"].pct_change(),
-        ret_strat=lambda x: x["ret"].mul(x["pos"]),
-    )
     return data
 
 
 @st.cache_data
-def back_test(data: pd.DataFrame, start_year: int = 2010) -> pd.DataFrame:
+def back_test(
+    data: pd.DataFrame, start_year: int = 2010, n_sma: int = 10
+) -> pd.DataFrame:
     """
     Perform a back test on financial data.
 
@@ -54,11 +55,16 @@ def back_test(data: pd.DataFrame, start_year: int = 2010) -> pd.DataFrame:
         return series.multiply(series.index >= start_dt).add(1).cumprod().sub(1)
 
     start_dt = pd.Timestamp(year=start_year, month=1, day=1)
-    # Calculate the first business day of the previous month
     first_prev_month_day = start_dt - pd.offsets.BMonthBegin(1)
 
     tbl = (
         data.tz_convert(None)
+        .assign(
+            sma=lambda x: x["Close"].rolling(window=n_sma).mean(),
+            pos=lambda x: (x["Close"].gt(x["sma"])).astype(int).shift(1),
+            ret=lambda x: x["Close"].pct_change(),
+            ret_strat=lambda x: x["ret"].mul(x["pos"]),
+        )
         .loc[first_prev_month_day:]
         .assign(
             bh=lambda df: _calc_cum_ret(df["ret"], start_dt),
@@ -68,9 +74,9 @@ def back_test(data: pd.DataFrame, start_year: int = 2010) -> pd.DataFrame:
     return tbl
 
 
-def create_cum_ret_chart(back_tested_data: pd.DataFrame, ticker: str) -> alt.Chart:
+def create_cum_ret_chart(back_tested_data: pd.DataFrame, ix_name: str) -> alt.Chart:
     chart_data = back_tested_data[["bh", "strat", "pos"]].reset_index()
-    title = f"{ticker}: Strategy Positions and Cumulative Return"
+    title = f"{ix_name}: Strategy Positions and Cumulative Return"
     chart = (
         alt.Chart(chart_data, title=title)
         .mark_point()
@@ -91,54 +97,34 @@ def create_cum_ret_chart(back_tested_data: pd.DataFrame, ticker: str) -> alt.Cha
     return chart
 
 
-def create_monthly_table(ts_monthly: pd.Series) -> pd.DataFrame:
-    """Create monthly performance table based on pandas.Series
-
-    It compounds (Daily) returns into monthly and creates monthly performance table.
-
-    Parameters
-    ----------
-    ts_monthly: pd.Series
-        monthly time series of returns in decimal format i.e. 0.01 = 1%
-
-    Returns
-    -------
-    tbl: pd.DataFrame
-        Calendar table with monthly and total returns
-
-    Examples
-    --------
-    >>> tbl = create_monthly_table(r)
-    >>> tbl.style.format("{:0.2%}", na_rep='').background_gradient(vmin=-1, vmax=1, cmap='RdBu', axis=None)  # noqa
-    """
-    monthly: pd.DataFrame = pd.DataFrame(
-        {
-            "ret": ts_monthly,
-            "Month": ts_monthly.index.month,
-            "Year": ts_monthly.index.year,
-        },
-        index=ts_monthly.index,
-    )
-    tbl = pd.pivot_table(monthly, values="ret", index=["Year"], columns=["Month"])
-    tbl["YTD"] = (1 + tbl).prod(axis=1) - 1
-    tbl = tbl.rename(columns=dict([(i, calendar.month_abbr[i]) for i in range(1, 13)]))
-    tbl.index = tbl.index.astype(str)
-    return tbl
-
-
 def main():
-    st.sidebar.title("Parameters")
-    ticker = st.sidebar.selectbox("Select Ticker", ["^GSPC", "^OMX", "^RUT"])
-    data = download_data(ticker)
+    with st.sidebar:
+        st.title("Parameters")
+        ix_name = st.sidebar.selectbox("Select Ticker", TICKER2NAME.keys())
+        ticker = TICKER2NAME[ix_name]
+        data = download_data(ticker)
 
-    start_year = st.sidebar.slider("Select Start Year", data.index[0].year, 2021, 2008)
-    back_tested_data = back_test(data, start_year)
+        start_year = st.sidebar.slider(
+            "Start Year", data.index[0].year, 2021, data.index[0].year + 1
+        )
+        n_sma = st.sidebar.number_input(
+            "SMA Period", min_value=2, max_value=24, value=10
+        )
+        bt_data = back_test(data, start_year, n_sma)
 
-    chart = create_cum_ret_chart(back_tested_data, ticker)
+    st.markdown(
+        """
+        # Monthly Trend-Following Strategy
+        
+        Discover the potential of a simple trend-following strategy, based on 
+        [Faber's 2006 academic research](https://papers.ssrn.com/sol3/papers.cfm?abstract_id=962461):
+
+        - Buy when monthly price > 10-month SMA
+        - Sell and move to cash when monthly price < 10-month SMA
+        """
+    )
+    chart = create_cum_ret_chart(bt_data, ix_name=ix_name)
     st.altair_chart(chart, use_container_width=True, theme=None)
-
-    tbl_perf = create_monthly_table(ts_monthly=back_tested_data["ret_strat"])
-    st.dataframe(tbl_perf.style.format("{:0.2%}", na_rep=""), use_container_width=True)
 
 
 if __name__ == "__main__":
